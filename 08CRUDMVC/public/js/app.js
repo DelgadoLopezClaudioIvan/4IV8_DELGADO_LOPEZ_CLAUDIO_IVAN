32eefc422a774434579afba5072d2abd420c1347
DelgadoLopezClaudioIvan <cdelgadol2400@alumno.ipn.mx>
2026-05-26 15:11:16 -0600

    prueba

---
+++ 08CRUDMVC/public/js/app.js
@@ -0,0 +1,3 @@
+/*
+vams a tener una estructura mediante la cual implementamos el modelo vista controlador, en donde a partir del uso de una Api podemos consumir los elementos por parte del front para obtener sus datos cmo peticion o como respuesta del back
+*/
