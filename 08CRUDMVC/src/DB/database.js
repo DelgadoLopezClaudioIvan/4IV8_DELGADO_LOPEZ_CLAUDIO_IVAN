e1651ce3e5ac3dc2e2d597d162fcb7f1cc94bb32
DelgadoLopezClaudioIvan <cdelgadol2400@alumno.ipn.mx>
2026-06-06 17:33:50 -0600

    ya porfin funciona esta cosa, entre comillas porq solo puerto 3000

--- 08CRUDMVC/src/DB/database.js
+++ 08CRUDMVC/src/DB/database.js
@@ -4,7 +4,6 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host : 'localhost',
-    port : 3306,
     user : 'root',
     password : 'root',
     database : 'practicacrud',
